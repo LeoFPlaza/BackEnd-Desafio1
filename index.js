ab6f876c810528b7b64665c2e046b6a81242bdfa
LeoFPlaza <leoplaz@gmail.com>
2023-11-07 22:25:49 -0300

    quito console logs innecesarios

--- index.js
+++ index.js
@@ -1,74 +1,58 @@
 /*
-
-
-class Product {
-    static lastCode = 0;
-    constructor(title, description, price, thumbnail, stock) {
-        this.title = title,
-            this.description = description,
-            this.price = price,
-            this.thumbnail = thumbnail,
-            this.code = this.setIdCode(),
-            this.stock = stock
-    }
-    setIdCode() {
-        let newCode = ++Product.lastCode;
-        return newCode;
-    }
-}
-
-
-class ProductManager {
-    static products = []
-    constructor(title, description, price, thumbnail, stock) {
-        this.Product = new Product(title, description, price, thumbnail, stock),
-            this.products = this.addProduct(this.Product)
-    }
-
-
-
-    getProducts() {
-        return ProductManager.products;
-    }
-
-    addProduct(newProd) {
-        ProductManager.products = [...ProductManager.products, newProd];
-    }
-
-    getProductById(id) {
-        let productFound = "Not Found"
-        ProductManager.products.forEach(element => {
-            if (element.code == id) {
-                productFound = element
-            }
-        });
-        return productFound
-    }
-}
-// console.log(ProductManager.getProducts)
-let producto1 = new ProductManager("Air Force 1", "Zapatillas Nike Azules de cuero", 121000, "thumbnail not available", 12)
-let producto2 = new ProductManager("Puma Roma", "Zapatillas Puma blancas y negras de cuero", 70000, "thumbnail not available", 22)
-let producto3 = new ProductManager("Adidas superstar", "Zapatillas Adidas Originals verdes y negras de cuero", 121000, "thumbnail not available", 34)
- console.log(producto3.getProductById(3))
  */
 
-/**/
-class Product {
-    static lastCode = 0;
-    constructor(title, description, price, thumbnail, stock) {
-        this.title = title,
-            this.description = description,
-            this.price = price,
-            this.thumbnail = thumbnail,
-            this.code = this.setIdCode(),
-            this.stock = stock
-    }
-    setIdCode() {
-        let newCode = ++Product.lastCode;
-        return newCode;
-    }
-}
+// class Product {
+//     static lastCode = 0;
+//     constructor(title, description, price, thumbnail, stock) {
+//         this.title = title,
+//             this.description = description,
+//             this.price = price,
+//             this.thumbnail = thumbnail,
+//             this.code = this.setIdCode(),
+//             this.stock = stock
+//     }
+//     setIdCode() {
+//         let newCode = ++Product.lastCode;
+//         return newCode;
+//     }
+// }
 
+
+// class ProductManager {
+//     static products = []
+//     constructor(title, description, price, thumbnail, stock) {
+//         this.Product = new Product(title, description, price, thumbnail, stock),
+//             this.products = this.addProduct(this.Product)
+//     }
+
+
+
+//     getProducts() {
+//         return ProductManager.products;
+//     }
+
+//     addProduct(newProd) {
+//         ProductManager.products = [...ProductManager.products, newProd];
+//     }
+
+//     getProductById(id) {
+//         let productFound = "Not Found"
+//         ProductManager.products.forEach(element => {
+//             if (element.code == id) {
+//                 productFound = element
+//             }
+//         });
+//         return productFound
+//     }
+// }
+// // console.log(ProductManager.getProducts)
+// let producto1 = new ProductManager("Air Force 1", "Zapatillas Nike Azules de cuero", 121000, "thumbnail not available", 12)
+// let producto2 = new ProductManager("Puma Roma", "Zapatillas Puma blancas y negras de cuero", 70000, "thumbnail not available", 22)
+// let producto3 = new ProductManager("Adidas superstar", "Zapatillas Adidas Originals verdes y negras de cuero", 121000, "thumbnail not available", 34)
+// // console.log(producto3.getProductById(132))
+
+/*
+ */
 
 class ProductManager {
     static lastCode = 0;
@@ -77,12 +61,15 @@ class ProductManager {
         this.products = ProductManager.products
     }
 
+
+
     getProducts() {
         return ProductManager.products;
     }
 
     addProduct(title, description, code, price, thumbnail, stock) {
         let newProd
+
         if (ProductManager.products.length > 0) {
             ProductManager.products.forEach(element => {
                 if (element.code == code) {
@@ -112,14 +99,14 @@ class ProductManager {
     }
 
     setIdCode() {
-        let newCode = ++Product.lastCode;
+        let newCode = ++ProductManager.lastCode;
         return newCode;
     }
 }
 
 
 let productManager = new ProductManager()
-// console.log(productManager.getProducts())
+console.log(productManager.getProducts())
 
 productManager.addProduct("producto prueba", "Este es un producto prueba", "ABC123", 200, "sin imagen", 25)
 // console.log(productManager.getProducts())
@@ -130,11 +117,10 @@ productManager.addProduct("producto prueba", "Este es un producto prueba", "ABC1
 console.log("")
 
 productManager.addProduct("producto prueba2", "Este es un producto prueba2", "ABC1232", 200, "sin imagen", 25)
-// console.log(productManager.getProducts())
-// console.log("")
-// console.log("")
-// console.log("producto:")
+console.log(productManager.getProducts())
+console.log("")
+console.log("")
+console.log("producto:")
 
 
-console.log(productManager.getProductById("ABC132"))
-
+console.log(productManager.getProductById("ABC32"))
